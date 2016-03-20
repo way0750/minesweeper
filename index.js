18a07ea1c141d6995ebf74fef108ac142e1a7108
way0750 <bruce0750@gmail.com>
2016-03-19 21:45:24 -0700

    switch port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 
 app.use(express.static('./public'));
 
-var port = process.env.port || 3000;
+var port = process.env.PORT || 3000;
 
 app.listen(port);
 console.log('server running at ', port);
